898e6327795c895c829723f98b2da254fabd49a7
Hao <tsunghaochou@gmail.com>
2026-01-20 10:28:59 +0800

    chore: normalize script

--- create_view.sql
+++ create_view.sql
@@ -1,18 +1,18 @@
-﻿USE [行政網路系統];
+﻿USE [<YourDatabaseName>];
 GO
 
-CREATE OR ALTER VIEW dbo.vVisit_WithVisitDT
+CREATE OR ALTER VIEW <YourViewName>
 AS
 SELECT
     t.*,
-    VisitDT =
+    <TargetColumnName> =
         TRY_CONVERT(datetime2(0),
             CONCAT(
-                CAST(CAST(LEFT(LTRIM(RTRIM(t.拜訪日期)), 3) AS int) + 1911 AS char(4)),
-                SUBSTRING(LTRIM(RTRIM(t.拜訪日期)), 4, LEN(LTRIM(RTRIM(t.拜訪日期))) - 3),
-                CASE WHEN LEN(LTRIM(RTRIM(t.拜訪日期))) = 15 THEN ':00' ELSE '' END
+                CAST(CAST(LEFT(LTRIM(RTRIM(t.<SourceColumnName>)), 3) AS int) + 1911 AS char(4)),
+                SUBSTRING(LTRIM(RTRIM(t.<SourceColumnName>)), 4, LEN(LTRIM(RTRIM(t.<SourceColumnName>))) - 3),
+                CASE WHEN LEN(LTRIM(RTRIM(t.<SourceColumnName>))) = 15 THEN ':00' ELSE '' END
             ),
             120
         )
-FROM dbo.外訪表 AS t;
+FROM <YourTableName> AS t;
 GO
